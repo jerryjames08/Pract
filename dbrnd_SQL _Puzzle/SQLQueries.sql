060497cad2c80980d0eea7db7567829f6bdc84fa
Jerry James <jerryjames08@outlook.com>
2018-06-19 18:40:23 +0530

    Puzzle -dbrnd

---
+++ dbrnd_SQL _Puzzle/SQLQueries.sql
@@ -0,0 +1,20 @@
+------List of Monday's
+
+DECLARE @month VARCHAR(10)='April',@year INT='2018',@day INT =01
+DECLARE 
+@LastDay INT=DAY(EOMONTH(CONCAT(@month,' ',@day,' ',@year)))
+
+--SELECT DAY(EOMONTH(CONCAT(@month,' ','30',' ',@year)))
+
+
+
+WHILE @day<=@LastDay
+BEGIN
+
+IF(DATEPART(DW,CONCAT(@month,' ',@day,' ',@year)))=2
+PRINT (CONVERT(DATE,CONCAT(@month,' ',@day,' ',@year)))
+
+SET @day=@day+1
+
+
+END
