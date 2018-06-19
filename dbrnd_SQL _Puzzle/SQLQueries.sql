e446cb47f7c4fe1dd81bb6e10ab624ac49c3652a
Jerry James <jerryjames08@outlook.com>
2018-06-19 19:39:05 +0530

    Added code
    Co-Authored-By: Jerry Richardson <jerry@jerryr.com>

--- dbrnd_SQL _Puzzle/SQLQueries.sql
+++ dbrnd_SQL _Puzzle/SQLQueries.sql
@@ -1,20 +1,30 @@
-------List of Monday's
+--------List of Monday's
 
-DECLARE @month VARCHAR(10)='April',@year INT='2018',@day INT =01
-DECLARE 
-@LastDay INT=DAY(EOMONTH(CONCAT(@month,' ',@day,' ',@year)))
+--DECLARE @month VARCHAR(10)='April',@year INT='2018',@day INT =01
+--DECLARE 
+--@LastDay INT=DAY(EOMONTH(CONCAT(@month,' ',@day,' ',@year)))
 
---SELECT DAY(EOMONTH(CONCAT(@month,' ','30',' ',@year)))
-
-
-
-WHILE @day<=@LastDay
-BEGIN
-
-IF(DATEPART(DW,CONCAT(@month,' ',@day,' ',@year)))=2
-PRINT (CONVERT(DATE,CONCAT(@month,' ',@day,' ',@year)))
-
-SET @day=@day+1
+----SELECT DAY(EOMONTH(CONCAT(@month,' ','30',' ',@year)))
 
 
-END
+
+--WHILE @day<=@LastDay
+--BEGIN
+
+--IF(DATEPART(DW,CONCAT(@month,' ',@day,' ',@year)))=2
+--PRINT (CONVERT(DATE,CONCAT(@month,' ',@day,' ',@year)))
+
+--SET @day=@day+1
+
+
+--END
+
+
+--------------------------GET Month Name-----------------
+
+--SELECT DATENAME(MONTH,GETDATE(0))
+
+
+-----------------------------------------------------------
+
+
